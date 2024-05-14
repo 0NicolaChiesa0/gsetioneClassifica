df2ec1e74d0374f6468840670a14dd753946fdc8
Nicola Chiesa <nicola.chiesa@marconi.loc>
2024-05-14 10:20:36 +0200

    ..

--- src/GestioneClassifica.java
+++ src/GestioneClassifica.java
@@ -22,15 +22,73 @@ public class GestioneClassifica {
         return posizione;
     }
 
-    public void setPersona(Persona p){
+    public void setPersona(Persona persona){
+        if (persona!=null){
+            int i = 0;
+            boolean trovato = false;
+            if (casellaLibera < classifica.length){
+                while (i <casellaLibera && !trovato){
+                    if (persona.getPunteggio()>classifica[i].getPunteggio()){
+                        for (int j = casellaLibera-1; j > i; j--){
+                            classifica[j+1] = classifica[j];
+                        }
+                        classifica[i] = persona;
+                        trovato = true;
+                    }
+                    i++;
+                }
+                if (!trovato){
+                    classifica[casellaLibera] = persona;
+                }
+                casellaLibera++;
+            }
+        }
+
+    }
+
+    public double calcoloMedia(){
+        double media = 0;
+        for (int i = 0; i < casellaLibera; i++){
+            media += classifica[i].getPunteggio();
+        }
+        media = media / casellaLibera;
+
+        return media;
+    }
+
+    public int persona2(Persona p){
+        int reult =-1;
         if(p!=null){
-            int i=0;
-            boolean trovato=false;
-            if(casellaLibera < classifica.length){
-                while (i<casellaLibera && !trovato) {
+            if(num_persone<10){
+                this.classifica[num_persone++] = new Persona(p);
+                int i =num_persone;
+                boolean trovato=false;
+                while(i>1 && !trovato){
+                    i--;
+                    if (classifica[i].getPunteggio()<classifica[i-1].getPunteggio()){
+                        Persona temp = classifica[i];
+                        classifica[i]=classifica[i-1];
+                        classifica[i-1]=temp;
+                        temp=null;
+                    }
                     
                 }
             }
         }
     }
+
+    public int killPersona(String nominativo){
+        int pos=cercaPersona(nominativo);
+        if(pos!=-1){
+            classifica[pos]=null;
+            for (int i = pos; i < classifica.length; i++) {
+                classifica[i]= classifica [i+1];
+            }
+            classifica[--num_persone]=null;
+        }
+        return pos;
+    }
+
+
+
 }
